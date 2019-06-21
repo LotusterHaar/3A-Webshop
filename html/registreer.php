8aef38a53222a3821f42a5f12dbfb8e92720b662
LotusTerHaar <lotus.ter.haar@gmail.com>
2019-06-21 20:27:00 +0200

    registratieformulier, waarden van velden worden opgeslagen in de sessionStorage

--- html/registreer.php
+++ html/registreer.php
@@ -2,37 +2,45 @@
 require_once('../protectedfunctions/generalfunctions.php');
 require_once('../protectedfunctions/dbfunctions.php');
 
-// Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
-
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $database = db_con('rw');
+    //$_SESSION['registration-error'] = "test";
+    //print_r($_SERVER);
+    //die();
+    echo ('<PRE>');
+    $_SESSION['Registerform-values'] = array();
+    foreach($_POST as $key => $value) {
+        echo "POST parameter '$key' has '$value'";
+        $_SESSION['Registerform-values'] = array_push_assoc($_SESSION['Registerform-values'], $key, $value );
+    }
 
     // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
+    if(empty(trim($_POST['username']))){
+        $_SESSION['registration-error'] = "Voer een gebruikersnaam in"; //  Browser should check this but just in case
+        toonRedirectHeader();
     } else{
+        $_SESSION['registration-error'] = "Voer een gebruikersnaam in"; //  Browser should check this but just in case
         // Prepare a select statement
-        $sql = "SELECT ID FROM user WHERE UserName = :username";
+        $sql = "SELECT ID FROM user WHERE UserName = :username and Deleted!=1;";
 
-        if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-
-            // Set parameters
-            $param_username = trim($_POST["username"]);
+        if($stmt = $database->prepare($sql)){
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
             // Attempt to execute the prepared statement
-            if($stmt->execute()){
+            if($stmt->execute(['username' => $_POST["username"]])){
                 if($stmt->rowCount() == 1){
-                    $username_err = "Deze gebruikersnaam is al bezet";
+                    $_SESSION['registration-error'] = "Deze gebruikersnaam is al bezet";
                 } else{
                     $username = trim($_POST["username"]);
                 }
             } else{
-                echo "Error: Er ging iets fout, probeer het later nog eens.";
+                $_SESSION['registration-error'] = "Er ging iets fout bij het aanmaken van de gebruiker, probeer het later nog eens.";
             }
+        }
+
+        if (isset($_SESSION['registration-error']) && !empty($_SESSION['registration-error']))
+        {
+            toonRedirectHeader();
         }
 
         // Close statement
@@ -59,7 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($_SESSION['registration-error']) && empty($password_err) && empty($confirm_password_err)){
 
         // Prepare an insert statement
         $sql = "INSERT INTO `user` (`UserName`, `FirstName`, `Prefix`, `Surname`, `Adress`, `HouseNumber`, `ZipCode`, `City`, `PhoneNumber`, `E-Mail`, `Gender`, `Avatar`, `Password`, `Deleted`) VALUES
@@ -89,12 +97,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     // Close connection
-    unset($pdo);
+    unset($database);
+    $database=null; #RESET database session
+}
+else {
+    include '../includes/header.html';
+    include './content/registratiepagina.html';
+    include '../includes/footer.html';
 }
 
+function toonRedirectHeader(){
+    unset($database);
+    $database=null; #RESET database session
 
-include '../includes/header.html';
-include './content/registratiepagina.html';
-include '../includes/footer.html';
+    if (isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER']))
+#echo ('--');
+        header ("Location: ".$_SERVER['HTTP_REFERER']);
+    else
+#echo ('--');
+        header ("Location: /");
+    exit;
+}
 
+function array_push_assoc($array, $key, $value){
+$array[$key] = $value;
+return $array;
+}
+
+function checkIfSetAndShow($key)
+{
+    if (isset($key) && !empty($key))
+        echo $key;
+}
 ?>
