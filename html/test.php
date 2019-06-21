c66323ffe8b4832ad63d84a84b6d916a66d7e09e
LotusTerHaar <lotus.ter.haar@gmail.com>
2019-06-21 10:12:38 +0200

    MainCategoryName -> CategoryName

--- html/test.php
+++ html/test.php
@@ -7,16 +7,21 @@ $sql = "SELECT `CategoryName`, `MainCategoryID` FROM `maincategory` ORDER BY `Ca
 $stmt = $database->prepare($sql);
 $stmt->execute();
 $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$database=null; #RESET database session
+$database = null; #RESET database session
 
 print ('<pre>');
 foreach ($categories as $categoryName) {
-    print ('Resolven van categorie: '.$categoryName['MainCategoryID']."\r\n");
+    print ('Resolven van categorie: ' . $categoryName['MainCategoryID'] . "\r\n");
     print_r($categoryName);
+
+    echo '<input id="tab-' . $categoryName['MainCategoryID'] . '" type="checkbox" name="tabs">';
+    echo('<label for="tab-' . $categoryName['MainCategoryID'] . '">' . $categoryName['CategoryName'] . '</label>');
+
+
     $sqlQuery = "SELECT * FROM `category` WHERE `MainCategoryID`=:categoryID ORDER BY  `CategoryOrder` ASC";
     $query = $database2->prepare($sqlQuery);
     $query->execute(['categoryID' => $categoryName['MainCategoryID']]);
-    print ('Subcategorie: '."\r\n");
+    print ('Subcategorie: ' . "\r\n");
     $subcategory = $query->fetchAll(PDO::FETCH_ASSOC);
     print_r($subcategory);
 }
