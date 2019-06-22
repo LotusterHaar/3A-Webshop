b6d8234c2d54906e9eb2b14c5f7652d4e794287a
janwil <Testfase175084>
2019-06-22 21:37:45 +0200

    Over ons aangepast

--- html/over-ons/index.php
+++ html/over-ons/index.php
@@ -1,5 +1,5 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
-include './content/over-ons.html';
+include $_SERVER['DOCUMENT_ROOT'].'/content/over-ons.html';
 include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
 ?>
