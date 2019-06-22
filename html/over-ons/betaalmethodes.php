e2935f7153ce11fda3f1fe1d584f443bb2004ac4
janwil <Testfase175084>
2019-06-22 21:40:22 +0200

    nieuws en betaalmethodes

--- html/over-ons/betaalmethodes.php
+++ html/over-ons/betaalmethodes.php
@@ -1,5 +1,5 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
-include './content/betaaalmethodes.html';
+include './content/betaalmethodes.html';
 include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
 ?>
