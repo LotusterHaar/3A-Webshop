d8c1b4a1632cab8ad7d9b4c83d266a0899b36955
janwil <Testfase175084>
2019-06-25 01:43:04 +0200

    Redirect beheren niet goed

--- html/webshop/producten/beheren.php
+++ html/webshop/producten/beheren.php
@@ -3,6 +3,6 @@ include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
 if (isAdmin())
     include '../content/productenbeheren.html';
 else
-    include $_SERVER['DOCUMENT_ROOT'].'/../content/over-ons.html';
+    include $_SERVER['DOCUMENT_ROOT'].'/content/over-ons.html';
 include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
 ?>
