989cdc067972c31d8e69c62852c103107c13c862
janwil <Testfase175084>
2019-06-30 00:55:06 +0200

    wacthwoordreset

--- html/webshop/wachtwoord-vergeten.php
+++ html/webshop/wachtwoord-vergeten.php
@@ -3,7 +3,31 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/../protectedfunctions/generalfunctions.
 require_once($_SERVER['DOCUMENT_ROOT'].'/../protectedfunctions/dbfunctions.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/../protectedfunctions/user.php');
 
-if ($_SERVER['REQUEST_METHOD']=="POST" && !isLoggedin()) {
+parse_str($_SERVER['QUERY_STRING'], $queryresolved);
+
+if (isset($queryresolved['token']) && !empty($queryresolved['token'])) {
+    $token=$queryresolved['token'];
+    $database = db_con();
+    $sql = "SELECT `ID` FROM `user` WHERE Deleted!=1 AND Token=:Token;";
+    $stmt = $database->prepare($sql);
+    $stmt->execute( [
+        ':Token' => $token,
+    ]);
+    $succesfull = $stmt->fetch(PDO::FETCH_ASSOC);
+    $database=null;
+    $userid=$succesfull['ID'];
+    if (!empty($userid)) {
+        $_SESSION['wachtwoordresettoken']=$token;
+    }
+
+    include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
+    include './content/wachtwoord-reset.html';
+    include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
+
+    die();
+
+}
+else if ($_SERVER['REQUEST_METHOD']=="POST" && !isLoggedin()) {
     $database = db_con();
     $database_rw = db_con('rw');
 
@@ -34,8 +58,8 @@ if ($_SERVER['REQUEST_METHOD']=="POST" && !isLoggedin()) {
                     $recovery = $stmt->fetch(PDO::FETCH_ASSOC);
                     $userid = $recovery['ID'];
                     $email = $recovery['EMail'];
-                    if (empty($recovery['RecoveryDate']) || (!empty($recovery['RecoveryDate']) && mktime() >= strtotime('+1 day', $recovery['RecoveryDate']))) {
-                        $recoverydate = mktime();
+                    if (empty($recovery['RecoveryDate']) || (!empty($recovery['RecoveryDate']) && time() >= strtotime('+1 day', $recovery['RecoveryDate']))) {
+                        $recoverydate = time();
                         $token = md5(uniqid('', true));
 
                         // Prepare an insert statement
@@ -58,11 +82,13 @@ if ($_SERVER['REQUEST_METHOD']=="POST" && !isLoggedin()) {
                                 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
                                 $message = '<html><body>';
-                                $message .= '<img src="http://localhost/images/logo_tuneshop_aqua.png" alt="Tuneshop Logo" />';
+                                $message .= '<img src="http://localhost/images/logo_tuneshop_aqua.png" alt="Tuneshop Logo" /><BR><BR>';
 
 
-                                $message .= '<A HREF="localhost/webshop/wachtwoord-vergeten?token=' . $token . "Klik op deze link om het wachtwoord te resetten</A>";
-                                $message .= "</body></html>";
+                                $message .= '<h1>Hallo,<br>U heeft aangegeven dat u uw wachtwoord vergeten bent.<br><br>';
+                                $message .= '<a href="http://'.$_SERVER['SERVER_NAME'].'/webshop/wachtwoord-vergeten?token=' . $token . '">Klik op deze link om het wachtwoord te resetten</a><br><br>';
+                                $message .= "Groeten en tot shops,<BR>Tuneshop.Online";
+                                $message .= "</h1</body></html>";
 
                                 if (!mail($email, "Tuneshop.online Wachtwoord Vergeten", $message, $headers)) {
                                     $_SESSION['registration-error'] = 'Er is een probleem opgetreden met versturen van de wachtwoord vergeten mail.';
@@ -77,15 +103,15 @@ if ($_SERVER['REQUEST_METHOD']=="POST" && !isLoggedin()) {
                         }
                     }
                     include $_SERVER['DOCUMENT_ROOT'] . '/../includes/header.html';
-                    include $_SERVER['DOCUMENT_ROOT'] . './content/over-ons.html';
+                    include $_SERVER['DOCUMENT_ROOT'] . '/content/over-ons.html';
                     include $_SERVER['DOCUMENT_ROOT'] . '/../includes/footer.html';
                     die();
 
                 }
-                }
+            }
         }
-    // Close statement
-    unset($stmt);
+        // Close statement
+        unset($stmt);
     }
     // Close connection
     unset($database);
@@ -96,28 +122,28 @@ if ($_SERVER['REQUEST_METHOD']=="POST" && !isLoggedin()) {
     $database_rw = null; #RESET database session
 
     include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
-    include $_SERVER['DOCUMENT_ROOT'].'./content/over-ons.html';
+    include $_SERVER['DOCUMENT_ROOT'].'/content/over-ons.html';
     include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
     die();
 }
-    include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
-    include './content/wachtwoord-vergeten.html';
-    include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
+include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
+include './content/wachtwoord-vergeten.html';
+include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
 
-    //Cleanup old errors
-    unset ($_SESSION['infobox']);
-    unset ($_SESSION['registration-error']);
-    unset ($_SESSION['Registerform-values']);
+//Cleanup old errors
+unset ($_SESSION['infobox']);
+unset ($_SESSION['registration-error']);
+unset ($_SESSION['Registerform-values']);
 
 function array_push_assoc($array, $key, $value){
-if (!empty(trim($value)))
-    $array[$key] = trim($value);
-return $array;
+    if (!empty(trim($value)))
+        $array[$key] = trim($value);
+    return $array;
 }
 
 function checkIfSetAndShow($key)
 {
     if (isset($_SESSION['Registerform-values'][$key]) && !empty($_SESSION['Registerform-values'][$key]))
-            echo $_SESSION['Registerform-values'][$key];
+        echo $_SESSION['Registerform-values'][$key];
 }
 ?>
