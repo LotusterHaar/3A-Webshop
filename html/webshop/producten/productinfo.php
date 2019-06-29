10c348f039b05c775a0a5100f836dcba322ad697
janwil <Testfase175084>
2019-06-29 03:14:28 +0200

    webshop automatisch naar producten

---
+++ html/webshop/producten/productinfo.php
@@ -0,0 +1,9 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/../protectedfunctions/generalfunctions.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/../protectedfunctions/dbfunctions.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/../protectedfunctions/user.php');
+
+include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
+include '../content/productoverzicht.html';
+include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
+?>
