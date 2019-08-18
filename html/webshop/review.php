f2ee754515519ed15a06740ed679d7fb5bfaeb48
LotusTerHaar <lotus.ter.haar@gmail.com>
2019-08-18 17:26:58 +0200

    link naar review pagina werkend gemaakt door een map review aan te maken met een index.php file en de css toe te voegen aan de reviewpagina.html pagina

---
+++ html/webshop/review.php
@@ -0,0 +1,8 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
+if (isLoggedin())
+    include './content/reviewpagina.html';
+else
+    include $_SERVER['DOCUMENT_ROOT'].'/../content/over-ons.html';
+include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
+?>
