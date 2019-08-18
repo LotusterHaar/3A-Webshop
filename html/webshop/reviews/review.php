f65a653988aca900ce493e8eb546fb242290279f
janwil <Testfase175084>
2019-08-18 22:13:28 +0200

    Review url

--- html/webshop/reviews/review.php
+++ html/webshop/reviews/review.php
@@ -3,7 +3,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/../includes/header.html';
 if (isLoggedin())
     include '../content/reviewpagina.html';
 else
-    include $_SERVER['DOCUMENT_ROOT'].'../../content/over-ons.html';
+    include $_SERVER['DOCUMENT_ROOT'].'../content/over-ons.html';
 include $_SERVER['DOCUMENT_ROOT'].'/../includes/footer.html';
 ?>
 
